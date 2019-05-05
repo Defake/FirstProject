fe0f16b452afbced6b74b48349accff158c2fe34
Anton <47733610+GeneralShtorks@users.noreply.github.com>
2019-05-05 20:25:56 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,9 +1,24 @@
 <html>
-<head>
-<title>CV Shirokov A.V.</title>
-</head>
 <body>
-<center>
-<h1>Резюме</h1>
-</center>
+<form method="POST">
+Number 1: <input type="text" name="number1">
+<br/>
+Number 2: <input type="text" name="number2">
+<br/>
+<input type="button" name="operation" value='+'/>
+<input type="button" name="operation" value='-'/>
+<input type="button" name="operation" value='*'/>
+<input type="button" name="operation" value='/'/>
+<br/>
+<?php
+$number1 = $_POST['number1'];
+$number2 = $_POST['number2'];
+$operation = $_POST['operation'];
+if ($answer=='+'){$answer = $number1 + $number2;}
+else if ($operation=='-'){$answer = $number1 - $number2;}
+else if ($operation=='*'){$answer = $number1 * $number2;}
+else if ($operation=='/'){$answer = $number1 / $number2;}
+?>
+<p>Результат вычиления равен: <?php echo "$answer" ; ?></p>
 </body>
+</html>
